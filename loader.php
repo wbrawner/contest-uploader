1f279b453afba98d5a9edb2bfd177b04fc2c433b
Billy Brawner <billybrawner@gmail.com>
2016-02-21 01:26:39 -0600

    updated copyright in loader.php

--- loader.php
+++ loader.php
@@ -8,7 +8,7 @@ Version: 1.0
 Author: William Brawner (Branau)
 Author URI: http://wbrawner.com
 License: GPL2
-Copyright 2015 William Brawner (email : billybrawner@gmail.com)
+Copyright 2016 William Brawner (Branau)(email : billybrawner@gmail.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License, version 2, as
